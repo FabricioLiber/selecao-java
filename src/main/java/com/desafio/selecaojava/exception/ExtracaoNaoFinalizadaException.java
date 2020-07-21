f5e671d239d268d7bc6ed2061de7e1963b7e8dc8
Fabrício Liberato <fabricio.liber@gmail.com>
2020-07-20 21:40:16 -0300

    Adiciona exceções para import de csv

---
+++ src/main/java/com/desafio/selecaojava/exception/ExtracaoNaoFinalizadaException.java
@@ -0,0 +1,9 @@
+package com.desafio.selecaojava.exception;
+
+public class ExtracaoNaoFinalizadaException extends Exception {
+
+    public ExtracaoNaoFinalizadaException (String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/desafio/selecaojava/exception/GravacaoDeDadosNaoFinalizadaException.java
@@ -0,0 +1,9 @@
+package com.desafio.selecaojava.exception;
+
+public class GravacaoDeDadosNaoFinalizadaException extends Exception {
+
+    public GravacaoDeDadosNaoFinalizadaException (String message) {
+        super(message);
+    }
+
+}
